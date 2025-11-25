048c3cc915a108e537161ef6f45583efd401c46f
Anurag Yadav <anuragyadav622003@gmail.com>
2025-11-25 21:45:14 +0530

    add loggerService

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { LoggerModule } from 'logger-service';
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      serviceName:'PayPulseBakend',
+      level:'debug'
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
